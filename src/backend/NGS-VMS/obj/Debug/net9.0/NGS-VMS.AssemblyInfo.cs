aca970b990cf0d03eb268b62e8c57fe23582c545
Aayush Shrivastava (ACOM Asian Resource Pte. Ltd.) <aayush@acom.com.sg>
2026-01-17 19:35:05 +0800

    Cleaned The Obj and Bin

--- src/backend/NGS-VMS/obj/Debug/net9.0/NGS-VMS.AssemblyInfo.cs
+++ src/backend/NGS-VMS/obj/Debug/net9.0/NGS-VMS.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NGS-VMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3a70c43263888450bc4e853fe624f8e6487b172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7254f39d969386216730d30d0a4424fd5df04a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NGS-VMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NGS-VMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
